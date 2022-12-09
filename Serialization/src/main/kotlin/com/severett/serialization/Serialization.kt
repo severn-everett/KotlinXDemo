4d4f11cf39ebc5b0a5c1ad1545c442039960308a
Severn Everett <severn.everett@gmail.com>
2022-12-09 12:36:28 +0100

    Added value class to serialization example

--- Serialization/src/main/kotlin/com/severett/serialization/Serialization.kt
+++ Serialization/src/main/kotlin/com/severett/serialization/Serialization.kt
@@ -6,10 +6,10 @@ import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 
 @Serializable
-data class Foo(val fizz: String, val bazz: List<String>)
+data class Foo(val fizz: String, val bazz: List<String>, val count: UInt)
 
 fun main() {
-    val jsonStr = Json.encodeToString(Foo("FUZZ", listOf("BIZZ", "BUZZ")))
+    val jsonStr = Json.encodeToString(Foo("FUZZ", listOf("BIZZ", "BUZZ"), 20u))
     println("ENCODED: $jsonStr")
     val newFoo = Json.decodeFromString<Foo>(jsonStr)
     println("DECODED: $newFoo")
